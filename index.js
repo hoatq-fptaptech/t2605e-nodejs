f8290c42497b953c88801f9d872372f7afa8940f
quanghoatrinh <quanghoa.trinh@gmail.com>
2026-08-11 20:08:44 +0700

    demo mongodb - create - get list product

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+// kết nối mongodb
+const stringconnection = "mongodb://localhost:27017/t2605e";
+const mongoose = require("mongoose");
+mongoose.connect(stringconnection).then(function(){
+    // Báo hiệu
+    console.log("Database connected!");
+})
+
+// thêm cấu hình nhận dữ liệu
+app.use(express.json());
 
 app.listen(PORT,function(){
     // Báo hiệu
@@ -13,4 +23,32 @@ app.get("/",function(req,res){
 });
 app.get("/about",(req,res)=>{
     res.send("About us");
+})
+app.post("/api/product", async (req,res)=>{
+    const data = req.body;
+    const Product = require("./models/product.model");
+    await Product.create(data);
+    res.send("CREATED");
+})
+app.get("/api/product",async (req,res)=>{
+    const Product = require("./models/product.model");
+    const list = await Product.find().exec();    
+
+    res.send(list);
+})
+app.get("/api/product/:id",(req,res)=>{
+    const x = req.params.id;
+    const data = {
+        value: x
+    };
+    res.json(data);
+})
+app.get("/api/category/:catId/product/:id",(req,res)=>{
+    const x = req.params.catId;
+    const y= req.params.id;
+    const data = {
+        v1: x,
+        v2: y
+    }
+    res.send(data);
 })
---
+++ models/product.model.js
@@ -0,0 +1,28 @@
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    // đinhj nghĩa các dữ liệu của 1 sản phẩm
+    title: {
+        // định nghĩa kỹ hơn cho thông tin này
+        type: String,
+        required: [true, "Please enter a valid title"]
+    },
+    // code:{
+    //     type:String,
+    //     unique: true
+    // }, 
+    price: {
+        type: Number,
+        required: true,
+        min:0
+    },
+    thumbnail: String,
+    description: String,
+    stock: {
+        type: Number,
+        required: true,
+        min:0
+    }
+})
+module.exports = mongoose.model("Product",productSchema);

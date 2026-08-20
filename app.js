8eff4f0ceabfaabdf918e6b3fb09d0403b32b7e8
quanghoatrinh <quanghoa.trinh@gmail.com>
2026-08-20 21:09:10 +0700

    cors

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+// cấu hình CORS
+const cors = require("cors");
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders: ["Content-Type","Authorization"]
+})
+);
 // thêm cấu hình nhận dữ liệu
 app.use(express.json());
 
